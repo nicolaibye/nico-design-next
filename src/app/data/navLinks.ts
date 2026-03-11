fd83661362dffd91961f19bcec51953209b27d1f
nicolaibye <nicbye02217@stud.noroff.no>
2026-03-11 17:35:00 +0100

    Tablet nav bar added

---
+++ src/app/data/navLinks.ts
@@ -0,0 +1,47 @@
+import {
+  HouseIcon,
+  KanbanIcon,
+  SmileyMeltingIcon,
+  QuotesIcon,
+} from "@phosphor-icons/react";
+import goldGif from "@/public/goldenBlob/goldenBlob_108.gif";
+
+export const navlinks = [
+  {
+    name: "Home",
+    href: "/",
+    icon: HouseIcon,
+    color: "bg-red-CoralRed",
+    hoverDistance: "hover:-translate-x-14.5",
+  },
+  {
+    name: "Project",
+    href: "/project",
+    icon: KanbanIcon,
+    color: "bg-green-RiverBed",
+    hoverDistance: "hover:-translate-x-20",
+  },
+  {
+    name: "Gold",
+    href: "/gold",
+    icon: null,
+    color: "bg-white",
+    image: goldGif,
+  },
+  {
+    name: "About",
+    href: "/about",
+    icon: SmileyMeltingIcon,
+    color: "bg-blue-NileBlue",
+    hoverDistance: "hover:-translate-x-16",
+  },
+  {
+    name: "Blog",
+    href: "/blog",
+    icon: QuotesIcon,
+    color: "bg-black-Mirage",
+    hoverDistance: "hover:-translate-x-13.5",
+  },
+];
+
+export default navlinks;
