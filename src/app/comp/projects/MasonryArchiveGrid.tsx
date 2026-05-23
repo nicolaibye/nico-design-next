b1968d48e8ca151fe822a4ceb88878e9eacf74ba
nicolaibye <nicbye02217@stud.noroff.no>
2026-05-23 22:32:11 +0200

    Adjusting and updating archiveGrid data and rendering along with fullscreen

--- src/app/comp/projects/MasonryArchiveGrid.tsx
+++ src/app/comp/projects/MasonryArchiveGrid.tsx
@@ -1,104 +1,68 @@
 import Image from "next/image";
+import { items } from "@/data/archiveData.ts";
+import { useState } from "react";
 
-type MasonryItem = {
-  id: string;
-  image: string;
-  alt: string;
-  title: string;
-  description?: string;
-};
+// Setup div with setState, transition with opactiy, getting item.image from click, showing the image from click in div for fullscreen.
 
-const items: MasonryItem[] = [
-  {
-    id: "1",
-    image:
-      "https://res.cloudinary.com/dg0c4lry9/image/upload/v1776091819/ios-app-icon-iphone-17-pro-mockup-on-gradient-background-v6-front-view_pnvyzj.jpg",
-    alt: "...",
-    title: "Title",
-    description: "Some text",
-  },
-  {
-    id: "2",
-    image:
-      "https://res.cloudinary.com/dg0c4lry9/image/upload/v1776091817/Floating-Classic-Notebooks-Cover-Design-psd-Mockup_eaeglf.jpg",
-    alt: "...",
-    title: "Title",
-    description: "Some text",
-  },
-  {
-    id: "3",
-    image:
-      "https://res.cloudinary.com/dg0c4lry9/image/upload/v1776091819/ios-app-icon-iphone-17-pro-mockup-on-gradient-background-v6-front-view_pnvyzj.jpg",
-    alt: "...",
-    title: "Title",
-    description: "Some text",
-  },
-  {
-    id: "4",
-    image:
-      "https://res.cloudinary.com/dg0c4lry9/image/upload/v1776091816/Studio_Desk_iPhone_Mockup_ysmdxf.jpg",
-    alt: "...",
-    title: "Title",
-    description: "Some text",
-  },
-    {
-    id: "5",
-    image:
-      "https://res.cloudinary.com/dg0c4lry9/image/upload/v1776091819/ios-app-icon-iphone-17-pro-mockup-on-gradient-background-v6-front-view_pnvyzj.jpg",
-    alt: "...",
-    title: "Title",
-    description: "Some text",
-  },
-    {
-    id: "3",
-    image:
-      "https://res.cloudinary.com/dg0c4lry9/image/upload/v1776091819/ios-app-icon-iphone-17-pro-mockup-on-gradient-background-v6-front-view_pnvyzj.jpg",
-    alt: "...",
-    title: "Title",
-    description: "Some text",
-  },
-  {
-    id: "4",
-    image:
-      "https://res.cloudinary.com/dg0c4lry9/image/upload/v1776091816/Studio_Desk_iPhone_Mockup_ysmdxf.jpg",
-    alt: "...",
-    title: "Title",
-    description: "Some text",
-  },
-    {
-    id: "5",
-    image:
-      "https://res.cloudinary.com/dg0c4lry9/image/upload/v1776091819/ios-app-icon-iphone-17-pro-mockup-on-gradient-background-v6-front-view_pnvyzj.jpg",
-    alt: "...",
-    title: "Title",
-    description: "Some text",
-  },
-];
+const MasonryArchiveGrid = () => {
+  const [activeImage, setActiveImage] = useState<string | null>(null);
 
-const MasonryArchiveGrid = () => (
-  <div className="columns-2 md:columns-3 gap-4 space-y-4 my-40 max-w-6xl px-10 sm:px-16 mx-auto">
-    {items.map((item) => (
-      <div key={item.id} className="break-inside-avoid">
-        <div className="rounded-lg overflow-hidden">
-          <Image
-            src={item.image}
-            alt={item.alt}
-            className="w-full h-auto object-cover"
-            width={1000}
-            height={1000}
-          />
-          <div className="p-3 dark:bg-green-RiverBed bg-black-Mirage">
-            <h3 className="font-lexend font-medium text-white-LinkWater dark:text-black-Mirage">{item.title}</h3>
-            {item.description && (
-              <p className="font-lexend text-white-LinkWater/60 dark:text-black-Mirage/60 mt-1">
-                {item.description}
-              </p>
-            )}
+  const fullscreenImage = (image: string) => {
+    setActiveImage(image);
+    document.body.classList.add("overflow-hidden");
+  };
+
+  const closeFullscreen = () => {
+    setActiveImage(null);
+    document.body.classList.remove("overflow-hidden");
+  };
+
+  return (
+    <>
+      <div className="columns-2 md:columns-3 gap-4 space-y-4 my-40 max-w-6xl px-10 sm:px-16 mx-auto">
+        {items.map((item) => (
+          <div
+            key={item.id}
+            className="break-inside-avoid"
+            onClick={() => fullscreenImage(item.image)}
+          >
+            <div className="rounded-lg overflow-hidden cursor-pointer">
+              <Image
+                src={item.image}
+                alt={item.alt}
+                className="w-full h-auto object-cover"
+                width={1000}
+                height={1000}
+              />
+              <div className="p-4 dark:bg-green-RiverBed bg-black-Mirage">
+                <h3 className="font-lexend font-medium text-white-LinkWater dark:text-black-Mirage leading-3">
+                  {item.title}
+                </h3>
+                {item.description && (
+                  <p className="font-lexend text-white-LinkWater/60 dark:text-black-Mirage/60 leading-3 mt-2">
+                    {item.description}
+                  </p>
+                )}
+              </div>
+            </div>
           </div>
-        </div>
+        ))}
       </div>
-    ))}
-  </div>
-);
+      <div
+        id="fullscreen"
+        onClick={closeFullscreen}
+        className={`fixed inset-0 bg-black-Mirage/80 flex items-center justify-center z-50 ${activeImage ? "opacity-100 pointer-events-auto" : "opacity-0 pointer-events-none"} z-9999`}
+      >
+        <Image
+          src={activeImage || "https://placehold.co/600x400/EEE/31343C"}
+          alt="Fullscreen"
+          className="p-20 w-auto h-full object-cover"
+          width={1000}
+          height={1000}
+        />
+      </div>
+    </>
+  );
+};
 
 export default MasonryArchiveGrid;
