9de4b1788e7febdc1e758f7e6c5f501d5b3a85b2
nicolaibye <nicbye02217@stud.noroff.no>
2026-07-28 18:31:46 +0200

    Added spinner to BlodCard

--- src/app/(website)/comp/blog/BlogCard.tsx
+++ src/app/(website)/comp/blog/BlogCard.tsx
@@ -14,6 +14,7 @@ type BlogPost = {
 };
 
 export const BlogCard = ({ post }: { post: BlogPost }) => {
+  const [isClicked, setIsClicked] = React.useState(false);
   const categoryArrows: Record<string, string> = {
     design: "bg-red-CoralRed",
     lifestyle: "bg-green-RiverBed",
@@ -21,11 +22,23 @@ export const BlogCard = ({ post }: { post: BlogPost }) => {
     other: "bg-black-Mirage",
   };
 
+  const categorySpinner: Record<string, string> = {
+    design: "border-red-CoralRed",
+    lifestyle: "border-green-RiverBed",
+    career: "border-blue-NileBlue",
+    other: "border-black-Mirage",
+  };
+
+  if (!post) {
+    return null;
+  }
+
   return (
     <li key={post.id} className="w-full sm:w-auto">
       <Link
         href={`/blog/${post.slug}`}
         className="flex flex-row items-center w-full sm:w-85 h-35 rounded-2xl bg-white overflow-hidden relative drop-shadow-[0_4px_6px_rgba(0,0,0,0.14)] lg:hover:scale-105"
+        onClick={() => setIsClicked(!isClicked)}
       >
         <Image
           src={post.cardImageUrl}
@@ -41,15 +54,22 @@ export const BlogCard = ({ post }: { post: BlogPost }) => {
           <p className="font-lexend font-light text-base capitalize">
             {post.category}
           </p>
-          <div
-            className={`flex items-center justify-center absolute bottom-2 right-2 ${categoryArrows[post.category] ?? "bg-black-Mirage"} rounded-full`}
-          >
-            <ArrowUpRightIcon
-              size={26}
-              weight="regular"
-              className={`text-white-LinkWater p-1.5`}
+
+          {!isClicked ? (
+            <div
+              className={`flex items-center justify-center absolute bottom-2 right-2 ${categoryArrows[post.category] ?? "bg-black-Mirage"} rounded-full`}
+            >
+              <ArrowUpRightIcon
+                size={26}
+                weight="regular"
+                className={`text-white-LinkWater p-1.5`}
+              />
+            </div>
+          ) : (
+            <div
+              className={`w-6.5 h-6.5 border-4 absolute bottom-2 right-2 ${categorySpinner[post.category] ?? "border-black-Mirage"} border-t-transparent rounded-full animate-spin`}
             />
-          </div>
+          )}
         </div>
       </Link>
     </li>
